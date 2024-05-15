0799b17ea4296316d5851415fff730fffab64637
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-15 15:54:34 +0200

    ser seeder and surname in users table

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+       /*  $users = [
+            [
+
+                "name" => 'Gian Marco',
+                "surname" => 'Pimentel',
+                "birth_date" => '1996/12/11',
+                "photo" => "",
+
+            ],
+            [
+
+                "name" => 'Vincenzo',
+                "surname" => 'Sergi',
+                "birth_date" => '1996/12/11',
+                "photo" => "",
+
+            ],
+            [
+
+                "name" => 'Arianna',
+                "surname" => 'Cosimini',
+                "birth_date" => '1996/12/11',
+                "photo" => "",
+
+            ],
+            [
+
+                "name" => 'Federico',
+                "surname" => 'Di Bitonto',
+                "birth_date" => '1996/12/11',
+                "photo" => "",
+
+            ],
+
+        ];
+        
+        foreach($users as $user) {
+            $newUser_data = new User();
+
+            $newUser_data->name = $user['name'];
+            $newUser_data->surname = $user['surname'];
+            $newUser_data->birth_date = $user['birth_date'];
+
+            $newUser_data->save();
+        } */
+    }
+}
