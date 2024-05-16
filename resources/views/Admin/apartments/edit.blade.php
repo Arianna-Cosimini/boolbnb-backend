9ae5a0a3c8208456b256307e634b51d730022bbc
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-16 16:24:32 +0200

    Feat:add error message in function validateform

--- resources/views/Admin/apartments/edit.blade.php
+++ resources/views/Admin/apartments/edit.blade.php
@@ -151,26 +151,26 @@
 
 
             <!-- <div class="mb-3">
-                                <label class="mb-2" for="">Categorie</label>
-                                <div class="d-flex gap-4">
+                                    <label class="mb-2" for="">Categorie</label>
+                                    <div class="d-flex gap-4">
 
-                                    @foreach ($categories as $category)
+                                        @foreach ($categories as $category)
     <div class="form-check d-flex flex-column justify-content-center align-items-center">
-                                        <label for="category-{{ $category->id }}" class="form-check-label"><i class="{{ $category->icon }}"></i></label>
-                                        <label for="category-{{ $category->id }}" class="form-check-label">{{ $category->title }}</label>
-                                        <input type="checkbox" name="categories[]" value="{{ $category->id }}" class="form-check-input" id="category-{{ $category->id }}"
-                                            
-                                            @if ($errors->any()) {{ in_array($category->id, old('categories', [])) ? 'checked' : '' }}
+                                            <label for="category-{{ $category->id }}" class="form-check-label"><i class="{{ $category->icon }}"></i></label>
+                                            <label for="category-{{ $category->id }}" class="form-check-label">{{ $category->title }}</label>
+                                            <input type="checkbox" name="categories[]" value="{{ $category->id }}" class="form-check-input" id="category-{{ $category->id }}"
+                                                
+                                                @if ($errors->any()) {{ in_array($category->id, old('categories', [])) ? 'checked' : '' }}
 
                         @else 
 
                         {{ $apartment->categories->contains($category) ? 'checked' : '' }} @endif
-                                        >
-                                    
-                                    </div>
+                                            >
+                                        
+                                        </div>
     @endforeach
-                                </div>
-                            </div> -->
+                                    </div>
+                                </div> -->
 
             <div class="mb-3">
                 <label class="mb-2" for="">Vuoi Sponsorizzare il tuo BnB?</label>
@@ -323,9 +323,13 @@
             });
             // Se l'indirizzo inserito non corrisponde mostro un messaggio di errore
             if (!addressMatched) {
-                alert("L'indirizzo inserito non corrisponde a uno dei suggerimenti.");
+                const errorMessage = "L'indirizzo inserito non corrisponde a uno dei suggerimenti.";
+                document.getElementById('address-error').textContent = errorMessage;
                 return false;
+            } else {
+                document.getElementById('address-error').textContent = "";
             }
+
 
             // Se l'indirizzo corrisponde invio il modulo
             return true;
