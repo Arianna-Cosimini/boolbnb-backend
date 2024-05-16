430d08238d95862f7d84c5d257a7f889e17bf702
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-16 12:58:58 +0200

    create slug coum in apartaments table

---
+++ database/migrations/2024_05_16_105538_add_slug_to_apartments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('apartments', function (Blueprint $table) {
+            // add slug
+            $table->string('slug')->unique()->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('apartments', function (Blueprint $table) {
+            // drop slug
+            $table->dropColumn('slug');
+        });
+    }
+};
