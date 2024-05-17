e094861d9b3f5c16f8d3cc5a07d4519f0c21a752
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-17 15:17:51 +0200

    feat:import db in aparttamentcontroller api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
     public function index(){
         // ci restituisc tutti gli appartamenti dal db
-        return "ciao";
+        $apartments=Apartment::all();
+        // dd($apartments);
+
+        return response()->json([
+            "success"=>true,
+            "results"=>$apartments
+        ]);
     }
 }
