13946b6765718cb6671f01c07424655f557e8d5d
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-17 17:05:24 +0200

    feat: add method show in apartment controller e add route in api.php

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -8,14 +8,39 @@ use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // ci restituisc tutti gli appartamenti dal db
-        $apartments=Apartment::all();
+        // $apartments=Apartment::all();
         // dd($apartments);
 
+        // $apartments=Apartment::paginate(5);
+        $apartments = Apartment::with(['user', 'message', 'view', 'services', 'categories', 'sponsorships'])->paginate(5);
+
         return response()->json([
-            "success"=>true,
-            "results"=>$apartments
+            "success" => true,
+            "results" => $apartments
         ]);
+    }
+
+
+    public function show($slug)
+    {
+        // $Apartment = Apartment::find($id);
+
+        $apartment = Apartment::with(['user', 'message', 'view', 'services', 'categories', 'sponsorships'])->where('slug', '=', $slug)->first();
+
+
+        if ($apartment) {
+            return response()->json([
+                "success" => true,
+                "apartment" => $apartment
+            ]);
+        } else {
+            return response()->json([
+                "success" => false,
+                "error" => "Progetto non trovato"
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ApartmentController;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/apartments', [ApartmentController::class,'index']);
+Route::get('/apartments', [ApartmentController::class, 'index']);
+
+//rotta per la show singola
+Route::get('/apartments/{slug}', [ApartmentController::class, 'show']);
