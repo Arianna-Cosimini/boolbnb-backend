efb0e668ac4e3197272aaef95e435076ea7a2830
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-17 13:55:45 +0200

    feat:in controllers>api>apartmentcontroller create function index

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    //
+    public function index(){
+        // ci restituisc tutti gli appartamenti dal db
+        return "ciao";
+    }
 }
