446c9fc7ebc563f5ffe882ba4cc6f12f6e196d32
federico-dibitonto <f.dibitonto94@gmail.com>
2024-05-21 15:23:22 +0200

    fixed service call api to front

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,22 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         // ci restituisc tutti gli appartamenti dal db
         // $apartments=Apartment::all();
         // dd($apartments);
 
         // $apartments=Apartment::paginate(5);
-        $apartments = Apartment::with(['user', 'message', 'view', 'services', 'categories', 'sponsorships'])->paginate(12);
+        $apartments = Apartment::with(['user', 'message', 'view', 'services', 'categories', 'sponsorships']);
+
+        if ($request->has('services')) {
+            $services= $request->input('services');
+            $servicesArr= explode(', ', $services);
+
+            $apartments->whereHas('services', function ($apartments) use ($servicesArr) {
+                $apartments->whereIn('service_id', $servicesArr);
+            });
+        }
+
+        $allApartments= $apartments->paginate(12);
+        $allServices = Service::all();
 
         return response()->json([
             "success" => true,
-            "results" => $apartments
+            "results" => $allApartments,
+            "services" => $allServices
         ]);
     }
 
