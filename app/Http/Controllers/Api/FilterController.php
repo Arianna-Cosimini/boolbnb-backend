63d807f7ef34397026142d62040f94d7e70b72ae
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-22 15:40:07 +0200

    fix: FilterController function index()

--- app/Http/Controllers/Api/FilterController.php
+++ app/Http/Controllers/Api/FilterController.php
@@ -3,15 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class FilterController extends Controller
 {
     //
     public function index(Request $request) {
-       return response()->json([
-        "success" =>true,
-        "results" => 'ciao',
-       ]);
+
+        // values of request
+        $filters = $request->all();
+
+        //default command to set 20 km
+        /* $radius = $filters['radius'] ?? 20000; */
+
+        $query = Apartment::with(['user', 'message', 'view', 'services', 'categories', 'sponsorships']);
+
+        if(isset($filters['rooms'])){
+            $query->where('rooms' , '>=' ,$filters['rooms']) ;
+        }
+
+        if(isset($filters['bed_number'])) {
+            $query->where('bed_number' , '>=' ,$filters['bed_number']) ;
+        }
+
+        $apartments = $query->get();
+
+        return response()->json([
+            "success" => true,
+            "results" => $apartments,
+        ]);
+
+
+       
     }
 }
