24c7be1c4081e79970bc18a422b4e83421169bba
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-23 22:47:13 +0200

    fix:correct button

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -7,8 +7,8 @@
       </ol>
       
     <h1 class="mb-4 fs-2">Dashboard</h1>
-    <a href="{{route('admin.apartments.index')}}" class="btn btn-danger button-red text-white">I tuoi annunci</a>
-    <a href="{{route('admin.messages.index')}}" class="btn btn-danger button-red text-white">I tuoi annunci</a>
+    <a href="{{route('admin.apartments.index')}}" class="btn btn-danger button-red text-white">I tuoi annunci</a> <br>
+    <a href="{{route('admin.messages.index')}}" class="btn btn-danger button-red text-white mt-3">I tuoi messaggi</a>
 
 </div>
 @endsection
