937aa57f63d0e46ec65a8d9d4f410a51e7b66d62
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-27 13:26:10 +0200

    add configuration in .env and create a payment controller

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Braintree\Gateway;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    protected $gateway;
+
+    public function __construct()
+    {
+        $this->gateway = new Gateway([
+            'environment' => env('BRAINTREE_ENV'),
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+        ]);
+    }
+}
