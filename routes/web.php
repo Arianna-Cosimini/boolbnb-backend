c98ecb92ad52c48ec1bef89efb3bd50909bfc4ef
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-30 18:51:02 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,9 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ApartmentController;
-use App\Http\Controllers\BraintreeController;
 use App\Http\Controllers\MessageController;
-use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SponsorshipController;
-use App\Http\Controllers\UserDatasController;
 use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
@@ -39,40 +36,34 @@ require __DIR__ . '/auth.php';
 Route::middleware(['auth', 'verified'])
     ->name('admin.')
     ->prefix('admin')
-    ->group(
-        function () {
+    ->group(function () {
 
-            // Route::get('/admin',[DashboardController::class,'index'])->name('admin');
-            // Route::get('/users',[DashboardController::class,'users'])->name('users');
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-            Route::get('/', [DashboardController::class, 'index'])->name('index');
+        // Braintree
+        Route::get('/sponsorships/payment/token', [SponsorshipController::class, 'getClientToken'])->name('payment.token');
+        Route::post('/sponsorships/payment/process', [SponsorshipController::class, 'processPayment'])->name('payment.process');
 
-            // Braintree
-            Route::get('/sponsorships/payment/token', 'SponsorshipController@getClientToken')->name('admin.payment.token');
-            Route::post('/sponsorships/payment/process', 'SponsorshipController@processPayment')->name('admin.payment.process');
+        Route::resource('apartments', ApartmentController::class)->parameters(['apartments' => 'apartment:slug']);
 
-            Route::resource('apartments', ApartmentController::class)->parameters(['apartments' => 'apartment:slug']);
+        Route::get('users', [DashboardController::class, 'users'])->name('users');
 
-            Route::get('users', [DashboardController::class, 'users'])->name('users');
+        // Rotta per le views
+        Route::resource('visited', ViewController::class);
 
-            //rotta per le views
-            Route::resource('visited', ViewController::class);
+        // Rotta per i messaggi
+        Route::resource('messages', MessageController::class);
+        Route::delete('admin/messages/{message}', [MessageController::class, 'destroy'])->name('messages.destroy');
 
-            //rotta per i messagges
-            Route::resource('messages', MessageController::class);
-            Route::delete('admin/messages/{message}', [MessageController::class, 'destroy'])->name('admin.messages.destroy');
+        // Rotta per il singolo messaggio
+        Route::get('messages/{id}', [MessageController::class, 'show'])->name('messages.show');
 
-            //rotta per il singolo messaggio
-            Route::get('messages/{id}', [MessageController::class, 'show'])->name('messages.show');
-
-
-            //rotta per la sponsorizzazione
-            Route::resource('sponsorships', SponsorshipController::class, ['except' => ['show']]);
-            //rotta per la modifica della sponsorizzazione
-            Route::get('sponsorships/{apartment_id}/{sponsorship_id}/edit', [SponsorshipController::class, 'edit'])->name('admin.sponsorships.edit');
-            //rotta per l'aggiornamento della sponsorizzazione
-            Route::put('sponsorships/{apartment_id}/{sponsorship_id}', [SponsorshipController::class, 'update'])->name('admin.sponsorships.update');
-        }
-    );
+        // Rotta per la sponsorizzazione
+        Route::resource('sponsorships', SponsorshipController::class, ['except' => ['show']]);
+        // Rotta per la modifica della sponsorizzazione
+        Route::get('sponsorships/{apartment_id}/{sponsorship_id}/edit', [SponsorshipController::class, 'edit'])->name('sponsorships.edit');
+        // Rotta per l'aggiornamento della sponsorizzazione
+        Route::put('sponsorships/{apartment_id}/{sponsorship_id}', [SponsorshipController::class, 'update'])->name('sponsorships.update');
+    });
 
 require __DIR__ . '/auth.php';
